f6130b915ff0774c81ca2d93ab35bcc1caa19ce8
Scott Griffis <scottgriffis@solutionary.com>
2017-03-09 11:16:49 -0600

    Initial commit

---
+++ src/main/java/com/firebirdcss/component/application_menu/Menu.java
@@ -0,0 +1,175 @@
+package com.firebirdcss.component.application_menu;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.lang.reflect.Method;
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * This class allows for the quick and simple setup of a menu driven application.
+ * <p>
+ * To use this object one must instantiate this class, from there it is as simple as calling the
+ * {@link #addChild(String, Method)} method to add static {@link Method}s to the menu, and if one
+ * wishes to have a menu option which leads to a new menu/sub-menu then you simply instantiate another
+ * of these menu objects and add it to the parent menu using the {@link #addChild(String, Menu)} method.
+ * <p>
+ * When creating the menu system using this object one does not need to worry about an exit option for the
+ * main menu, nor does one need to worry about "Back" options for the sub-menus as that is already taken care
+ * of by this object.
+ * <p>
+ * Once the menu has been built out as desired one simply calls the {@link #display()} method on the main
+ * menu to begin execution of the menu system. At that point the menu system handles everything from there.
+ * 
+ * @author Scott Griffis
+ *
+ */
+public class Menu {
+	private BufferedReader inputSource;
+	private boolean menuActive = false;
+	protected final List<Object/*MethodOrMenu*/> menuItems = new ArrayList<>();
+	protected final List<String> menuItemsText = new ArrayList<>(); 
+	private String promptText = "Please select an option from the above menu:";
+	
+	/**
+	 * CONSTRUCTOR: Initial menu-item(s) added to the menu and inputSource stored for later use.
+	 * 
+	 * @param inputSource - The source from where the menu system will get user input as {@link BufferedReader}
+	 */
+	public Menu(BufferedReader inputSource) {
+		this.inputSource = inputSource;
+		this.menuItems.add("exit");
+		this.menuItemsText.add("Exit Application");
+		
+	}
+	
+	/**
+	 * Executes the desired menu item.
+	 * This may mean displaying a new sub-menu or execution of a {@link Method}
+	 * 
+	 * @param menuSelection - The desired menu selection as {@link Integer}
+	 */
+	public void executeMenuSelection(Integer menuSelection) {
+		Object selection = menuSelection != null ? this.menuItems.get(menuSelection - 1) : null;
+		
+		/* Take appropriate action on menu item */
+		if (selection != null) {
+			if (selection instanceof Menu) {
+				Menu menu = (Menu) selection;
+				menu.display();
+			} else if (selection instanceof Method) {
+				Method method = (Method) selection;
+				try {
+					method.invoke(null);
+				} catch (Exception e) {
+					System.err.println("ERROR: Unable to execute menu option!");
+				}
+			} else if (selection instanceof String) {
+				if (((String) selection).equals("exit")) this.menuActive = false;
+			}
+		} else {
+			System.out.println("Invalid selection; Try again!");
+		}
+	}
+	
+	/**
+	 * Changes the default menu choice prompt to a given one.
+	 * 
+	 * @param newPrompt - The new prompt text as {@link String}
+	 */
+	public void changeMenuChoicePrompt(String newPrompt) {
+		this.promptText = newPrompt;
+	}
+	
+	/**
+	 * Used to add a method related menu-item to this menu.
+	 * 
+	 * @param menuItemText - The text to be displayed in the menu which represents this item
+	 * @param method - The method to execute if this menu option is chosen as {@link Method}
+	 */
+	public void addChild(String menuItemText, Method method) {
+		this.menuItems.add(0, method);
+		this.menuItemsText.add(0, menuItemText);
+	}
+	
+	/**
+	 * Used to add a sub-menu related menu-item to this menu.
+	 * 
+	 * @param menuItemText - The text to be displayed in the menu which represents this item
+	 * @param subMenu - The sub-menu to execute if this menu option is chosen as {@link Menu}
+	 */
+	public void addChild(String menuItemText, Menu subMenu) {
+		this.menuItems.add(0, subMenu);
+		this.menuItemsText.add(0, menuItemText);
+		if (subMenu.menuItems.contains("exit")) {
+			subMenu.menuItemsText.set(subMenu.menuItems.indexOf("exit"), "Previous Menu");
+		} else {
+			subMenu.menuItems.add("exit");
+			subMenu.menuItemsText.add("Previous Menu");
+		}
+	}
+	
+	/**
+	 * This method causes this menu to be executed such that it is displayed for the user and
+	 * receives menu choices from the user.
+	 */
+	public void display() {
+		this.menuActive = true;
+		while (menuActive) {
+			System.out.println(renderMenu());
+			System.out.print(promptText + " ");
+			getMenuSelection();
+		}
+	}
+	
+	/**
+	 * PRIVATE METHOD: This private method generates the display of the menu based on available menu options.
+	 * 
+	 * @return Returns the rendered menu text as a {@link String}
+	 */
+	private String renderMenu() {
+		StringBuilder result = new StringBuilder();
+		result.append("\n\n\n")
+			.append(renderSeparator('=', 100)).append('\n')
+			.append(renderSeparator(' ', 48)).append("MENU\n")
+			.append(renderSeparator('=', 100)).append("\n\n");
+		for (int i = 1; i <= this.menuItemsText.size(); i++) {
+			result.append(i).append(") ").append(this.menuItemsText.get(i - 1)).append('\n');
+		}
+			
+		return result.toString();
+	}
+	
+	/**
+	 * PRIVATE METHOD: This private method renders a separator made from the specified repeating 
+	 * of a single character.
+	 * 
+	 * @param character - The character to make the separator from as <code>char</code>
+	 * @param length - The length of the separator in characters as <code>int</code>
+	 * @return Returns the rendered separator text as a {@link String}
+	 */
+	private String renderSeparator(char character, int length) {
+		StringBuilder result = new StringBuilder(length);
+		for (int i = 1; i <= length; i++) result.append(character);
+		
+		return result.toString();
+	}
+	
+	/**
+	 * PRIVATE METHOD: Gets and executes the appropriate menu item.
+	 */
+	private void getMenuSelection() {
+		String line = null;
+		try {
+			line = inputSource.readLine();
+		} catch (IOException e1) {
+			e1.printStackTrace();
+		}
+		
+		try {
+			this.executeMenuSelection(Integer.valueOf(line));
+		} catch (NumberFormatException e) {
+			this.executeMenuSelection(null);
+		}
+	}
+} // END OF CLASS
---
+++ src/test/java/com/firebirdcss/component/application_menu/demo/DemoApplication.java
@@ -0,0 +1,159 @@
+package com.firebirdcss.component.application_menu.demo;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+import com.firebirdcss.component.application_menu.Menu;
+
+/**
+ * This class makes up the DEMO for the {@link Menu} class.
+ * <p>
+ * The point of this demo is to allow others to see the Menu class in action as well as provide a 
+ * coding example for how to use the Menu class.
+ * 
+ * @author Scott Griffis
+ *
+ */
+public class DemoApplication {
+	private static BufferedReader bReader = null;
+	
+	/**
+	 * The main entry point of execution for this DEMO Application.
+	 * 
+	 * @param args - Arguments are not used for this application.
+	 * 
+	 * @throws NoSuchMethodException
+	 * @throws SecurityException
+	 */
+	public static void main(String[] args) throws NoSuchMethodException, SecurityException {
+		try (BufferedReader reader = new BufferedReader(new InputStreamReader(System.in))) {
+			bReader = reader;
+			
+			Menu mainMenu = buildMenus();
+			mainMenu.display(); // This is what causes the menu to actually begin running.
+			
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+		
+		System.out.println("\n~ Application Ended ~");
+	}
+	
+	/**
+	 * All building out of the {@link Menu} is done in this method so that it is all kept togather
+	 * and easy for others to find.
+	 * <p>
+	 * This is example code for how to properally build out a menu system using the {@link Menu} class.
+	 * 
+	 * @return Returns the built menu for execution externally of this method, as {@link Menu}
+	 * 
+	 * @throws NoSuchMethodException
+	 * @throws SecurityException
+	 */
+	public static Menu buildMenus() throws NoSuchMethodException, SecurityException {
+		Menu mainMenu = new Menu(bReader);
+		
+		Menu subMenuOne = new Menu(bReader);
+		subMenuOne.addChild("Menu Item 'C'", DemoApplication.class.getMethod("menuItemC"));
+		subMenuOne.addChild("Menu Item 'D'", DemoApplication.class.getMethod("menuItemD"));
+		subMenuOne.addChild("Menu Item 'E'", DemoApplication.class.getMethod("menuItemE"));
+		subMenuOne.addChild("Menu Item 'F'", DemoApplication.class.getMethod("menuItemF"));
+		
+		Menu subMenuTwo = new Menu(bReader);
+		subMenuTwo.addChild("Menu Item 'G'", DemoApplication.class.getMethod("menuItemG"));
+		
+		mainMenu.addChild("A sub-menu of multiple options", subMenuOne);
+		mainMenu.addChild("Menu Item 'A'", DemoApplication.class.getMethod("menuItemA"));
+		mainMenu.addChild("Menu Item 'B'", DemoApplication.class.getMethod("menuItemB"));
+		mainMenu.addChild("Another sub-menu", subMenuTwo);
+		
+		return mainMenu;
+	}
+	
+	/* =======================================[vv Method menu-items below vv]======================================= */
+	
+	/**
+	 * Method for use as a Menu-Item...
+	 */
+	public static void menuItemA() {
+		System.out.println("Entered menuItemA()");
+		try {
+			Thread.sleep(3000);
+		} catch (InterruptedException e) {
+			e.printStackTrace();
+		}
+	}
+	
+	/**
+	 * Method for use as a Menu-Item...
+	 */
+	public static void menuItemB() {
+		System.out.println("Entered menuItemB()");
+		try {
+			Thread.sleep(3000);
+		} catch (InterruptedException e) {
+			e.printStackTrace();
+		}
+	}
+	
+	/**
+	 * Method for use as a Menu-Item...
+	 */
+	public static void menuItemC() {
+		System.out.println("Entered menuItemC()");
+		try {
+			Thread.sleep(3000);
+		} catch (InterruptedException e) {
+			e.printStackTrace();
+		}
+	}
+	
+	/**
+	 * Method for use as a Menu-Item...
+	 */
+	public static void menuItemD() {
+		System.out.println("Entered menuItemD()");
+		try {
+			Thread.sleep(3000);
+		} catch (InterruptedException e) {
+			e.printStackTrace();
+		}
+	}
+	
+	/**
+	 * Method for use as a Menu-Item...
+	 */
+	public static void menuItemE() {
+		System.out.println("Entered menuItemE()");
+		try {
+			Thread.sleep(3000);
+		} catch (InterruptedException e) {
+			e.printStackTrace();
+		}
+	}
+	
+	/**
+	 * Method for use as a Menu-Item...
+	 */
+	public static void menuItemF() {
+		System.out.println("Entered menuItemF()");
+		try {
+			Thread.sleep(3000);
+		} catch (InterruptedException e) {
+			e.printStackTrace();
+		}
+	}
+	
+	/**
+	 * Method for use as a Menu-Item...
+	 */
+	public static void menuItemG() {
+		System.out.println("Entered menuItemG()");
+		try {
+			Thread.sleep(3000);
+		} catch (InterruptedException e) {
+			e.printStackTrace();
+		}
+	}
+}
